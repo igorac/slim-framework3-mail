e06e21a2f1d62734400dce0f3bcfbedf37ad0f94
Igor AC <igorac1999@gmail.com>
2019-12-07 10:52:55 -0300

    Added project slim with phpmailer

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+require dirname(__DIR__) . "/bootstrap.php";
+
+ini_set('xdebug.overload_var_dump', 1);
+
+$app->get('/', 'App\controllers\HomeController:index');
+$app->get('/cadastro', 'App\controllers\CadastroController:create');
+$app->post('/cadastro/store', 'App\controllers\CadastroController:store');
+$app->get('/user/edit/{id}', 'App\controllers\UserController:edit');
+$app->put('/user/update/{id}', 'App\controllers\UserController:update');
+$app->delete('/user/delete/{id}', 'App\controllers\UserController:delete');
+$app->get('/contato', 'App\controllers\ContatoController:index');
+$app->post('/contato/store', 'App\controllers\ContatoController:store');
+
+$app->run();
